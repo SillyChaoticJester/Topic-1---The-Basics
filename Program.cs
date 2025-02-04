ff4ad29c83d9c9b88dc8782c3aecdaa25eb638a8
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-04 14:13:27 -0500

    Finished Problem 1, figuring out Problem 2

--- Program.cs
+++ Program.cs
@@ -7,9 +7,24 @@
             //Miracle McMahon   
             string firstName = "Miracle";
             string favMovie = "The Matrix";
-            
+            string favQuote = "Uh oh! Bad decision, Mark!";
+            favMovie = favMovie.ToUpper();
+            favMovie = favMovie.Replace("A", "@");
+            favMovie = favMovie.Replace("E", "3");
+            favQuote = favQuote.Replace("a", "");
+            favQuote = favQuote.Replace("A", "");
+            favQuote = favQuote.Replace("e", "");
+            favQuote = favQuote.Replace("E", "");
+            favQuote = favQuote.Replace("i", "");
+            favQuote = favQuote.Replace("I", "");
+            favQuote = favQuote.Replace("o", "");
+            favQuote = favQuote.Replace("O", "");
+            favQuote = favQuote.Replace("u", "");
+            favQuote = favQuote.Replace("U", "");
+
             Console.WriteLine($"My name is {firstName.ToLower()} and my favorite movie is {favMovie}");
             Console.WriteLine(favMovie.Contains("THE"));
+            Console.WriteLine(favQuote);
         }
     }
 }
